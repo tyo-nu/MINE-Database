ce61509d8662dc1940a8da439b3171fee2909885
James Jeffryes <jamesgjeffryes@gmail.com>
2015-10-30 08:52:55 -0500

    partial query testing impl

---
+++ Tests/test_queries.py
@@ -0,0 +1,43 @@
+__author__ = 'JGJeffryes'
+import queries
+import databases
+
+test_db = databases.MINE('mongotest')
+test_molfile = open("./Tests/xanthine.mol", "r").read()
+glucose = {'Names': ['Hexose', 'D-Idose', 'Glucose', 'Mannose', 'D-Gulose', 'D-Allose', 'D-Hexose', 'Dextrose',
+                     'Seminose', 'L-Gulose', 'D-Talose', 'D-Aldose', 'D-Mannose', 'D-Aldose2', 'D-Aldose1', 'D-Glucose',
+                     'D-Altrose', 'Carubinose', 'Grape sugar', 'L-Galactose', 'D-Galactose', 'D-ido-Hexose',
+                     'D-gulo-Hexose', 'D-talo-Hexose', 'beta-D-Mannose', 'beta-D-Glucose', 'D-altro-Hexose',
+                     'alpha-D-Glucose', 'alpha-D-Mannose', 'D-glucopyranose', 'beta-D-Galactose', 'alpha-D-Galactose',
+                     'D-galactopyranose', '1,4-beta-D-Mannooligosaccharide'], 'SMILES': 'OCC1OC(O)C(C(C1O)O)O',
+           'Generation': 0.0, 'Mass': 180.063388104, '_id': 'Cb5b3273ab083d77ed29fbef8f7e464929af29c13',
+           'MINE_id': 19160, 'NP_likeness': 0, 'Formula': 'C6H12O6', 'Inchikey': 'WQZGKKKJIJFFOK-UHFFFAOYSA-N'}
+glucose_id = {'_id': 'Cb5b3273ab083d77ed29fbef8f7e464929af29c13'}
+
+def test_quick_search():
+    assert queries.quick_search(test_db, 'WQZGKKKJIJFFOK-GASJEMHNSA-N') == [glucose]
+    assert queries.quick_search(test_db, 'C00031') == [glucose]
+    assert glucose in queries.quick_search(test_db, 'Glucose')
+    assert queries.quick_search(test_db, 'WQZGKKKJIJFFOK-GASJEMHNSA-N', {'_id': 1}) == [glucose_id]
+
+
+def test_database_query():
+    assert queries.advanced_search('admin', '') == ['Illegal query']
+    assert queries.advanced_search(test_db, "{'MINE_id': 19160}") == [glucose]
+    assert queries.advanced_search(test_db, "{'Names': 'Glucose'}") == [glucose]
+    assert queries.advanced_search(test_db, "{'MINE_id': 19160}", {'_id': 1}) == [glucose_id]
+
+def test_similarity_search():
+    raise NotImplemented
+    #assert len(queries.similarity_search(test_db, 'OC1OC(O)C(C(C1O)O)O', 0.9, "FP2", 100)) == 28
+    #print(len(queries.similarity_search(test_db, 'OCC1OC(O)C(C(C1O)O)O', 0.9, "FP2", 100)))
+    #assert len(queries.similarity_search(test_db, test_molfile, 0.8, 'FP4', 100)) == 7
+
+def test_substructure_search():
+    raise NotImplemented
+    #assert len(queries.substructure_search(test_db, 'cccccc', 100)) == 100
+    #assert isinstance(queries.substructure_search(test_db, 'Nc1ncnc2[nH]cnc12', 100)[0], dict)
+
+def test_structure_search():
+    assert queries.structure_search(test_db, 'OCC1OC(O)C(C(C1O)O)O') == [glucose]
+    assert queries.structure_search(test_db, test_molfile, {'_id': 1}) == [{'_id': 'C84d297bb12c40a0996e449dfc54afd69ccc3dd54'}]
