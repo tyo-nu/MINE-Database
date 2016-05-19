ff7ac77acc55494c8f2d50441134ffff10f3a9e2
James Jeffryes <jamesgjeffryes@gmail.com>
2016-05-19 08:48:17 -0500

    establishing adduct searching tests

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from rdkit.Chem import AllChem
 import hashlib
 from os import path
 
+
 def convert_sets_to_lists(obj):
     """Recursively converts dictionaries that contain sets to lists"""
     if isinstance(obj, set):
@@ -17,6 +18,7 @@ def convert_sets_to_lists(obj):
             obj[key] = convert_sets_to_lists(obj[key])
     return obj
 
+
 def get_dotted_field(input_dict, accessor_string):
     """Gets data from a dictionary using a dotted accessor-string"""
     current_data = input_dict
@@ -24,11 +26,13 @@ def get_dotted_field(input_dict, accessor_string):
         current_data = current_data.get(chunk, {})
     return current_data
 
+
 def save_dotted_field(accessor_string, data):
     """Gets data from a dictionary using a dotted accessor-string"""
     for chunk in accessor_string.split('.')[::-1]:
         data = {chunk: data}
     return data
+
 
 def memoize(f):
     """ Memoization decorator for a function taking one or more arguments. """
@@ -40,6 +44,7 @@ def memoize(f):
             ret = self[key] = f(*key)
             return ret
     return memodict().__getitem__
+
 
 def prevent_overwrite(write_path):
     """
@@ -57,6 +62,54 @@ def prevent_overwrite(write_path):
         else:
             write_path += '_new'
     return write_path
+
+
+def approximate_matches(list1, list2, epsilon=0.01):
+    """
+    Takes two list of tuples and searches for matches of tuples first value within the supplied epsilon. Emits tuples
+    with the tuples second values where found. if a value in one dist does not match the other list, it is emitted alone.
+    :param list1: first list of tuples
+    :type list1: list
+    :param list2: second list of tuples
+    :type list2: list
+    :param epsilon: maximum difference in
+    :type epsilon: float
+    :return: second values of tuples
+    :rtype: generator
+    """
+    list1.sort()
+    list2.sort()
+    list1_index = 0
+    list2_index = 0
+
+    while list1_index < len(list1) or list2_index < len(list2):
+        if list1_index == len(list1):
+            yield (0, list2[list2_index][1])
+            list2_index += 1
+            continue
+        if list2_index == len(list2):
+            yield (list1[list1_index][1], 0)
+            list1_index += 1
+            continue
+
+        list1_element = list1[list1_index][0]
+        list2_element = list2[list2_index][0]
+
+        difference = abs(list1_element - list2_element)
+
+        if difference < epsilon:
+            yield (list1[list1_index][1], list2[list2_index][1])
+            list1_index += 1
+            list2_index += 1
+        elif list1_element < list2_element:
+            yield (list1[list1_index][1], 0)
+            list1_index += 1
+        elif list2_element < list1_element:
+            yield (0, list2[list2_index][1])
+            list2_index += 1
+        else:
+            raise AssertionError('Unexpected else taken')
+
 
 def dict_merge(finaldict, sourcedict):
     """Merges two dictionaries using sets to avoid duplication of values"""
@@ -81,6 +134,7 @@ def dict_merge(finaldict, sourcedict):
             if not key in finaldict:
                 finaldict[key] = {}
             dict_merge(finaldict[key], val)
+
 
 def do_profile(func):
     from line_profiler import LineProfiler
