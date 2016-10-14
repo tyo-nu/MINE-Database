a1ceb7ff71cd8ddf4ce0526bb09aed1cf9ce77b0
James Jeffryes <jamesgjeffryes@gmail.com>
2016-10-13 18:41:37 -0700

    new rxns from excel function

---
+++ Scripts/add_rxns_from_excel.py
@@ -0,0 +1,54 @@
+from rdkit.Chem import AllChem
+from minedatabase.databases import MINE
+from minedatabase import utils
+import pandas as pd
+import xlrd
+import subprocess
+import os
+import sys
+
+
+def load_cdmine_rxns(mine_db, excel_file, pic_dir=""):
+    workbook = xlrd.open_workbook(excel_file)
+    abrv_sheet = workbook.sheet_by_index(1)
+    abrv = {}
+    if pic_dir and not os.path.exists(pic_dir):
+        os.mkdir(pic_dir)
+    compounds = pd.read_excel(excel_file, 1)
+    reactions = pd.read_excel(excel_file, 0)
+
+    for i, row in compounds.iterrows():
+        if row['SMILES'] == row['SMILES']:
+            mol = AllChem.MolFromSmiles(row['SMILES'])
+            if mol:
+                c_id = mine_db.insert_compound(mol, {"Generation": 0})
+                abrv[row['Abbreviation'].strip()] = c_id
+                if pic_dir:
+                    rc = subprocess.call(["/Applications/ChemAxon/JChem/bin/molconvert", "-o", pic_dir, "temp.png",
+                                          "png:-a,w500", "-s", "'%s'" % row['SMILES'].strip()], shell=True)
+                    if not rc:
+                        os.rename(pic_dir + "temp.png", pic_dir + c_id + ".png")
+            else:
+                print("Failed to parse %s" % row['SMILES'])
+        else:
+            print('SMILES missing from %s' % row.name)
+
+    reactions['Type of Reaction'].fillna('ffill', inplace=True)
+    for i, row in reactions.iterrows():
+        if row['Equation (Abbreviations)'] == row['Equation (Abbreviations)']:
+            rxn = row[['Metabolite', 'Type of Reaction', 'Equation (full names)', 'Description and Notes']].to_dict()
+            if isinstance(row['PMID or doi'], str):
+                rxn['References'] = row['PMID or doi'].strip().split('; ')
+            else:
+                rxn['References'] = [str(row['PMID or doi'])]
+
+            rxn['Notes'] = str(row[6]).strip()
+            rxn['Reactants'], rxn['Products'] = utils.parse_text_rxn(row['Equation (Abbreviations)'], ' = ', ' + ', abrv)
+            rxn['_id'] = utils.rxn2hash(rxn['Reactants'], rxn['Products'])
+            mine_db.reactions.save(rxn)
+        else:
+            print('RXN missing from %s' % row.name)
+
+if __name__ == '__main__':
+    mine = MINE(sys.argv[1])
+    load_cdmine_rxns(mine, sys.argv[2])
