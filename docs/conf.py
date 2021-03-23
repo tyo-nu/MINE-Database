ad4fa8c710f81d664705b70621eaf0b70412752c
Jon Strutz <jonstrutz11@gmail.com>
2021-03-23 12:48:05 -0500

    Updated docs - filters, intro, support, layout

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = 'Tyo Lab'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "numpydoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosectionlabel", "numpydoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/support.rst
+++ docs/support.rst
@@ -1,6 +1,6 @@
 Support
 =======
 
-How to contact us
+Need help? Found a bug? Have an idea for a useful feature?
 
-Open an issue on github
+Feel free to open up an issue at https://github.com/tyo-nu/MINE-Database for any of these situations, and we will get back to you as soon as we can!
