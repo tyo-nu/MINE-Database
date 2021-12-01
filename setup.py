4f9a4bf9d163e96da25a4dc8e5dffc9530fdc427
Jon Strutz <jonstrutz11@gmail.com>
2021-12-01 11:06:04 -0600

    Added 2.1.0 to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='minedatabase',
-      version='2.0.0',
+      version='2.1.0',
       description='Metabolic In silico Network Expansions',
       long_description=long_description,
       long_description_content_type="text/markdown",
